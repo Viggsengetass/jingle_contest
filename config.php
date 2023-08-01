b7f8213e3427884e32377e67a9dd703404d2253c
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-01 12:26:04 +0200

    congig_up

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'g4_genshin');
-define('DB_PASSWORD', 'g4_genshin');
-define('DB_NAME', 'g4_genshin');
+define('DB_USERNAME', 'jingle');
+define('DB_PASSWORD', 'jingle');
+define('DB_NAME', 'jingle');
 
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
