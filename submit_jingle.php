368385fba4bf034665b365cc2ced45a72d2db87f
Viggsengetass <paulito.antoine@gmail.com>
2023-08-04 16:23:21 +0200

    update_submit

--- submit_jingle.php
+++ submit_jingle.php
@@ -2,63 +2,68 @@
 session_start();
 require_once('config.php');
 
-// Vérifiez si l'utilisateur est connecté en tant qu'élève
-if ($_SESSION['role'] !== 'student') {
-    // Redirigez l'utilisateur vers la page de connexion s'il n'est pas un élève
+// Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
     exit();
 }
 
-// Traitement du formulaire d'envoi du jingle
+// Traitement du formulaire de soumission de jingle
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $jingle_title = $_POST['jingle_title'];
+    $jingle_file = $_FILES['jingle_file'];
 
-    // Vérifiez si un fichier a été téléchargé
-    if (isset($_FILES['jingle_file']) && $_FILES['jingle_file']['error'] === UPLOAD_ERR_OK) {
-        $jingle_file_path = 'uploads/' . $_FILES['jingle_file']['name'];
+    // Vérifiez si le fichier a été correctement téléchargé
+    if ($jingle_file['error'] === UPLOAD_ERR_OK) {
+        $jingle_tmp_name = $jingle_file['tmp_name'];
+        $jingle_name = basename($jingle_file['name']);
+        $jingle_path = 'data' . $jingle_name;
 
-        // Déplacez le fichier téléchargé vers le répertoire des téléchargements
-        move_uploaded_file($_FILES['jingle_file']['tmp_name'], $jingle_file_path);
+        // Déplacez le fichier téléchargé vers son emplacement final
+        if (move_uploaded_file($jingle_tmp_name, $jingle_path)) {
+            // Enregistrez les informations du jingle dans la base de données
+            $user_id = $_SESSION['user_id'];
+            $submission_date = date('Y-m-d H:i:s');
 
-        // Enregistrez les détails du jingle dans la table "jingles"
-        $student_id = $_SESSION['user_id'];
-        $submission_date = date('Y-m-d H:i:s');
+            $sql = "INSERT INTO jingles (student_id, jingle_title, jingle_file_path, submission_date) 
+                    VALUES ('$user_id', '$jingle_title', '$jingle_path', '$submission_date')";
 
-        $sql = "INSERT INTO jingles (student_id, jingle_title, jingle_file_path, submission_date)
-                VALUES ('$student_id', '$jingle_title', '$jingle_file_path', '$submission_date')";
-        $result = mysqli_query($conn, $sql);
-
-        if ($result) {
-            $success_message = "Votre jingle a été soumis avec succès !";
+            if (mysqli_query($conn, $sql)) {
+                header('Location: student_dashboard.php');
+                exit();
+            } else {
+                $error_message = "Une erreur est survenue lors de l'enregistrement du jingle.";
+            }
         } else {
-            $error_message = "Une erreur s'est produite lors de la soumission de votre jingle. Veuillez réessayer.";
+            $error_message = "Une erreur est survenue lors du téléchargement du fichier.";
         }
     } else {
-        $error_message = "Veuillez sélectionner un fichier pour votre jingle.";
+        $error_message = "Une erreur est survenue lors du téléchargement du fichier.";
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Déposer un jingle</title>
+    <title>Soumettre un jingle</title>
 </head>
 <body>
-<h1>Déposer un jingle</h1>
-<?php if (isset($error_message)) { ?>
-    <p style="color: red;"><?php echo $error_message; ?></p>
-<?php } elseif (isset($success_message)) { ?>
-    <p style="color: green;"><?php echo $success_message; ?></p>
-<?php } ?>
+<h1>Soumettre un jingle</h1>
 <form method="post" action="submit_jingle.php" enctype="multipart/form-data">
     <label for="jingle_title">Titre du jingle :</label>
     <input type="text" name="jingle_title" required><br>
+
     <label for="jingle_file">Fichier du jingle :</label>
-    <input type="file" name="jingle_file" accept=".mp3" required><br>
-    <input type="submit" value="Déposer le jingle">
+    <input type="file" name="jingle_file" required><br>
+
+    <input type="submit" value="Soumettre">
 </form>
-<a href="student_dashboard.php">Retour au tableau de bord des élèves</a>
-<a href="contest_stats.php">Voir les statistiques du concours</a>
-<a href="logout.php">Se déconnecter</a>
+<p><a href="student_dashboard.php">Retour au tableau de bord</a></p>
+<?php
+if (isset($error_message)) {
+    echo "<p>{$error_message}</p>";
+}
+?>
 </body>
 </html>
