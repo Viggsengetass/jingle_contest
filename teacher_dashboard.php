1f0c3a75c22d0516a6ff423f66df521eb4276b75
Viggsengetass <paulito.antoine@gmail.com>
2023-08-07 16:58:38 +0200

    fix_teacger_dashboard

--- teacher_dashboard.php
+++ teacher_dashboard.php
@@ -27,17 +27,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         mysqli_query($conn, $update_query);
     }
 }
+
+// Traitement de la soumission de commentaire
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['jingle_id']) && isset($_POST['comment'])) {
+        $jingle_id = $_POST['jingle_id'];
+        $comment = $_POST['comment'];
+        $teacher_id = $_SESSION['user_id'];
+
+        // Insertion du commentaire dans la table comments
+        $insert_query = "INSERT INTO comments (jingle_id, teacher_id, comment) 
+                         VALUES ('$jingle_id', '$teacher_id', '$comment')";
+        mysqli_query($conn, $insert_query);
+    }
+}
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Tableau de bord professeur</title>
-</head>
-<body>
-<h1>Tableau de bord professeur</h1>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Tableau de bord professeur</title>
+    </head>
+    <body>
+    <h1>Tableau de bord professeur</h1>
 
-<!-- Afficher la liste des jingles soumis par les élèves -->
+    <!-- Afficher la liste des jingles soumis par les élèves -->
 <?php
 $query = "SELECT j.*, AVG(r.score) AS average_score 
           FROM jingles j 
@@ -65,6 +79,14 @@ if (mysqli_num_rows($result) > 0) {
         echo "<input type='submit' value='Noter'>";
         echo "</form>";
 
+        // Formulaire de commentaire sur le jingle
+        echo "<form method='post' action='teacher_dashboard.php'>";
+        echo "<input type='hidden' name='jingle_id' value='{$row['jingle_id']}'>";
+        echo "<label for='comment'>Commentaire :</label>";
+        echo "<textarea name='comment' rows='3' required></textarea>";
+        echo "<input type='submit' value='Commenter'>";
+        echo "</form>";
+
         // Afficher les commentaires sur le jingle
         $comment_query = "SELECT c.*, u.first_name, u.last_name 
                           FROM comments c 
@@ -85,28 +107,28 @@ if (mysqli_num_rows($result) > 0) {
 }
 ?>
 
-<!-- Formulaire pour créer un compte élève -->
-<h2>Créer un compte élève</h2>
-<form method="post" action="register_student.php">
-    <label for="first_name">Prénom :</label>
-    <input type="text" name="first_name" required><br>
+    <!-- Formulaire pour créer un compte élève -->
+    <h2>Créer un compte élève</h2>
+    <form method="post" action="register_student.php">
+        <label for="first_name">Prénom :</label>
+        <input type="text" name="first_name" required><br>
 
-    <label for="last_name">Nom :</label>
-    <input type="text" name="last_name" required><br>
+        <label for="last_name">Nom :</label>
+        <input type="text" name="last_name" required><br>
 
-    <label for="username">Nom d'utilisateur :</label>
-    <input type="text" name="username" required><br>
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" name="username" required><br>
 
-    <label for="password">Mot de passe :</label>
-    <input type="password" name="password" required><br>
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" required><br>
 
-    <label for="email">Email :</label>
-    <input type="email" name="email" required><br>
+        <label for="email">Email :</label>
+        <input type="email" name="email" required><br>
 
-    <input type="submit" value="Créer le compte élève">
-</form>
+        <input type="submit" value="Créer le compte élève">
+    </form>
 
-<!-- Lien pour se déconnecter -->
-<a href='logout.php'>Se déconnecter</a>
-</body>
+    <!-- Lien pour se déconnecter -->
+    <a href='logout.php'>Se déconnecter</a>
+    </body>
 </html>
