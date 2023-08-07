fea27e1bb9948d9d5e4367a8b17dab5744b62ab3
Viggsengetass <paulito.antoine@gmail.com>
2023-08-07 11:59:41 +0200

    init_register_student

--- teacher_dashboard.php
+++ teacher_dashboard.php
@@ -4,59 +4,78 @@ require_once('config.php');
 
 // Vérifiez si l'utilisateur est connecté en tant que professeur
 if ($_SESSION['role'] !== 'teacher') {
-    // Redirigez l'utilisateur vers la page de connexion s'il n'est pas un professeur
     header('Location: login.php');
     exit();
 }
 
-// Vous pouvez ajouter ici le code pour afficher les élèves inscrits et leurs jingles soumis
-
+// Traitement de la notation d'un jingle
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['evaluate_jingle'])) {
+    if (isset($_POST['jingle_id']) && isset($_POST['score'])) {
         $jingle_id = $_POST['jingle_id'];
-        $evaluation = $_POST['evaluation'];
+        $score = $_POST['score'];
 
-        // Mettez à jour l'évaluation du jingle dans la table "jingles"
-        $sql = "UPDATE jingles SET evaluation = '$evaluation' WHERE jingle_id = $jingle_id";
-        $result = mysqli_query($conn, $sql);
-
-        if ($result) {
-            $success_message = "L'évaluation du jingle a été enregistrée avec succès !";
-        } else {
-            $error_message = "Une erreur s'est produite lors de l'enregistrement de l'évaluation. Veuillez réessayer.";
-        }
+        // Effectuez ici la mise à jour de la notation dans la base de données
+        // Utilisez la valeur de $jingle_id pour identifier le jingle et $score pour la note attribuée
+        // Mettez à jour la table jingles avec la note donnée par le professeur
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tableau de bord des professeurs</title>
+    <title>Tableau de bord professeur</title>
 </head>
 <body>
-<h1>Bienvenue, <?php echo $_SESSION['username']; ?> !</h1>
-<!-- Affichez ici les jingles soumis par les élèves avec des formulaires d'évaluation -->
+<h1>Tableau de bord professeur</h1>
+
+<!-- Afficher la liste des jingles soumis par les élèves -->
 <?php
-// Récupérez tous les jingles soumis par les élèves à partir de la table "jingles"
-$sql = "SELECT jingles.*, students.first_name, students.last_name FROM jingles
-            INNER JOIN students ON jingles.student_id = students.student_id";
-$result = mysqli_query($conn, $sql);
+$query = "SELECT * FROM jingles";
+$result = mysqli_query($conn, $query);
 
 if (mysqli_num_rows($result) > 0) {
-    echo "<h2>Évaluer les jingles soumis par les élèves :</h2>";
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<p>{$row['jingle_title']} par {$row['first_name']} {$row['last_name']} - <a href='{$row['jingle_file_path']}' target='_blank'>Écouter</a></p>";
+        echo "<p>Jingle soumis par l'élève {$row['student_id']}: {$row['jingle_title']}</p>";
+        echo "<audio controls>";
+        echo "<source src='{$row['jingle_file_path']}' type='audio/mpeg'>";
+        echo "Votre navigateur ne prend pas en charge l'élément audio.";
+        echo "</audio>";
+
+        // Formulaire de notation du jingle
         echo "<form method='post' action='teacher_dashboard.php'>";
         echo "<input type='hidden' name='jingle_id' value='{$row['jingle_id']}'>";
-        echo "Évaluation : <input type='number' name='evaluation' min='0' max='10' required>";
-        echo "<input type='submit' name='evaluate_jingle' value='Évaluer'>";
+        echo "<label for='score'>Note :</label>";
+        echo "<input type='number' name='score' min='0' max='10' required>";
+        echo "<input type='submit' value='Noter'>";
         echo "</form>";
     }
 } else {
-    echo "<p>Aucun jingle soumis par les élèves pour le moment.</p>";
+    echo "<p>Aucun jingle soumis pour le moment.</p>";
 }
 ?>
-<a href="register_student.php">Inscrire un nouvel élève</a>
-<a href="logout.php">Se déconnecter</a>
+<!-- Formulaire pour créer un compte élève -->
+<h2>Créer un compte élève</h2>
+<form method="post" action="register_student.php">
+    <label for="first_name">Prénom :</label>
+    <input type="text" name="first_name" required><br>
+
+    <label for="last_name">Nom :</label>
+    <input type="text" name="last_name" required><br>
+
+    <label for="username">Nom d'utilisateur :</label>
+    <input type="text" name="username" required><br>
+
+    <label for="password">Mot de passe :</label>
+    <input type="password" name="password" required><br>
+
+    <label for="email">Email :</label>
+    <input type="email" name="email" required><br>
+
+    <input type="submit" value="Créer le compte élève">
+</form>
+
+<!-- Lien pour se déconnecter -->
+<a href='logout.php'>Se déconnecter</a>
 </body>
 </html>
