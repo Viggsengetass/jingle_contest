3734ca8bb1aa97be234e9963cef63ade4e332202
Viggsengetass <paulito.antoine@gmail.com>
2023-08-07 15:20:33 +0200

    fix_student_dashboard

--- student_dashboard.php
+++ student_dashboard.php
@@ -7,6 +7,33 @@ if ($_SESSION['role'] !== 'student') {
     header('Location: login.php');
     exit();
 }
+
+// Traitement de la soumission du jingle
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $student_id = $_SESSION['user_id'];
+    $jingle_title = $_POST['jingle_title'];
+    $submission_date = date('Y-m-d H:i:s');
+
+    // Vérification du fichier téléchargé
+    if ($_FILES['jingle_file']['error'] === UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES['jingle_file']['tmp_name'];
+        $file_name = basename($_FILES['jingle_file']['name']);
+        $file_path = "uploads/{$file_name}";
+
+        // Déplacer le fichier vers le répertoire de destination
+        if (move_uploaded_file($tmp_name, $file_path)) {
+            // Insérer le jingle dans la base de données
+            $insert_query = "INSERT INTO jingles (student_id, jingle_title, jingle_file_path, submission_date) 
+                             VALUES ('$student_id', '$jingle_title', '$file_path', '$submission_date')";
+            mysqli_query($conn, $insert_query);
+            $success_message = "Jingle soumis avec succès !";
+        } else {
+            $error_message = "Une erreur est survenue lors du téléchargement du fichier.";
+        }
+    } else {
+        $error_message = "Une erreur est survenue lors du téléchargement du fichier.";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -37,6 +64,18 @@ if (mysqli_num_rows($result) > 0) {
 }
 ?>
 
+<!-- Formulaire pour soumettre un jingle -->
+<h2>Soumettre un jingle</h2>
+<form method="post" action="student_dashboard.php" enctype="multipart/form-data">
+    <label for="jingle_title">Titre du jingle :</label>
+    <input type="text" name="jingle_title" required><br>
+
+    <label for="jingle_file">Fichier du jingle (format MP3) :</label>
+    <input type="file" name="jingle_file" accept="audio/mpeg" required><br>
+
+    <input type="submit" value="Soumettre le jingle">
+</form>
+
 <!-- Afficher les notes attribuées au jingle soumis par l'élève -->
 <?php
 $query = "SELECT r.score 
@@ -57,5 +96,14 @@ if (mysqli_num_rows($result) > 0) {
 
 <!-- Lien pour se déconnecter -->
 <a href='logout.php'>Se déconnecter</a>
+
+<?php
+if (isset($error_message)) {
+    echo "<p>{$error_message}</p>";
+}
+if (isset($success_message)) {
+    echo "<p>{$success_message}</p>";
+}
+?>
 </body>
 </html>
