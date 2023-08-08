b6adbf9b94aa35557e31a6acb1b404014537405d
Viggsengetass <paulito.antoine@gmail.com>
2023-08-08 12:10:30 +0200

    fix_teacher_dashboard

--- teacher_dashboard.php
+++ teacher_dashboard.php
@@ -2,56 +2,70 @@
 session_start();
 require_once('config.php');
 
-// Vérifiez si l'utilisateur est connecté en tant que professeur
+// Vérifier le rôle de l'utilisateur
 if ($_SESSION['role'] !== 'teacher') {
     header('Location: login.php');
     exit();
 }
 
 // Traitement de la notation d'un jingle
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['jingle_id']) && isset($_POST['score'])) {
-        $jingle_id = $_POST['jingle_id'];
-        $score = $_POST['score'];
-        $teacher_id = $_SESSION['user_id'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['jingle_id']) && isset($_POST['score'])) {
+    $jingle_id = $_POST['jingle_id'];
+    $score = $_POST['score'];
+    $teacher_id = $_SESSION['user_id'];
 
-        // Insertion de la notation dans la table ratings
-        $insert_query = "INSERT INTO ratings (jingle_id, teacher_id, score) 
-                         VALUES ('$jingle_id', '$teacher_id', '$score')";
-        mysqli_query($conn, $insert_query);
+    // Insertion de la notation dans la table ratings
+    $insert_query = "INSERT INTO ratings (jingle_id, teacher_id, score) 
+                     VALUES ('$jingle_id', '$teacher_id', '$score')";
+    mysqli_query($conn, $insert_query);
 
-        // Calcul de la note moyenne et mise à jour dans la table jingles
-        $update_query = "UPDATE jingles SET average_rating = 
-                         (SELECT AVG(score) FROM ratings WHERE jingle_id = '$jingle_id') 
-                         WHERE jingle_id = '$jingle_id'";
-        mysqli_query($conn, $update_query);
-    }
+    // Calcul de la note moyenne et mise à jour dans la table jingles
+    $update_query = "UPDATE jingles SET average_rating = 
+                     (SELECT AVG(score) FROM ratings WHERE jingle_id = '$jingle_id') 
+                     WHERE jingle_id = '$jingle_id'";
+    mysqli_query($conn, $update_query);
 }
 
 // Traitement de la soumission de commentaire
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['jingle_id']) && isset($_POST['comment'])) {
-        $jingle_id = $_POST['jingle_id'];
-        $comment = $_POST['comment'];
-        $teacher_id = $_SESSION['user_id'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['jingle_id']) && isset($_POST['comment'])) {
+    $jingle_id = $_POST['jingle_id'];
+    $comment = $_POST['comment'];
+    $teacher_id = $_SESSION['user_id'];
 
-        // Insertion du commentaire dans la table comments
-        $insert_query = "INSERT INTO comments (jingle_id, teacher_id, comment) 
-                         VALUES ('$jingle_id', '$teacher_id', '$comment')";
-        mysqli_query($conn, $insert_query);
+    // Insertion du commentaire dans la table comments
+    $insert_query = "INSERT INTO comments (jingle_id, teacher_id, comment) 
+                     VALUES ('$jingle_id', '$teacher_id', '$comment')";
+    mysqli_query($conn, $insert_query);
+}
+
+// Fonction pour afficher les commentaires
+function displayComments($conn, $jingle_id) {
+    $comment_query = "SELECT c.*, u.first_name, u.last_name 
+                      FROM comments c 
+                      INNER JOIN users u ON c.teacher_id = u.user_id
+                      WHERE c.jingle_id = '$jingle_id'";
+    $comment_result = mysqli_query($conn, $comment_query);
+
+    if (mysqli_num_rows($comment_result) > 0) {
+        echo "<h3>Commentaires et retours :</h3>";
+        while ($comment_row = mysqli_fetch_assoc($comment_result)) {
+            echo "<p>De : {$comment_row['first_name']} {$comment_row['last_name']}</p>";
+            echo "<p>{$comment_row['comment']}</p>";
+        }
     }
 }
+
 ?>
 
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Tableau de bord professeur</title>
-    </head>
-    <body>
-    <h1>Tableau de bord professeur</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tableau de bord professeur</title>
+</head>
+<body>
+<h1>Tableau de bord professeur</h1>
 
-    <!-- Afficher la liste des jingles soumis par les élèves -->
+<!-- Afficher la liste des jingles soumis par les élèves -->
 <?php
 $query = "SELECT j.*, AVG(r.score) AS average_score 
           FROM jingles j 
@@ -88,47 +102,35 @@ if (mysqli_num_rows($result) > 0) {
         echo "</form>";
 
         // Afficher les commentaires sur le jingle
-        $comment_query = "SELECT c.*, u.first_name, u.last_name 
-                          FROM comments c 
-                          INNER JOIN users u ON c.teacher_id = u.user_id
-                          WHERE c.jingle_id = '{$row['jingle_id']}'";
-        $comment_result = mysqli_query($conn, $comment_query);
-
-        if (mysqli_num_rows($comment_result) > 0) {
-            echo "<h3>Commentaires et retours :</h3>";
-            while ($comment_row = mysqli_fetch_assoc($comment_result)) {
-                echo "<p>De : {$comment_row['first_name']} {$comment_row['last_name']}</p>";
-                echo "<p>{$comment_row['comment']}</p>";
-            }
-        }
+        displayComments($conn, $row['jingle_id']);
     }
 } else {
     echo "<p>Aucun jingle soumis pour le moment.</p>";
 }
 ?>
 
-    <!-- Formulaire pour créer un compte élève -->
-    <h2>Créer un compte élève</h2>
-    <form method="post" action="register_student.php">
-        <label for="first_name">Prénom :</label>
-        <input type="text" name="first_name" required><br>
+<!-- Formulaire pour créer un compte élève -->
+<h2>Créer un compte élève</h2>
+<form method="post" action="register_student.php">
+    <label for="first_name">Prénom :</label>
+    <input type="text" name="first_name" required><br>
 
-        <label for="last_name">Nom :</label>
-        <input type="text" name="last_name" required><br>
+    <label for="last_name">Nom :</label>
+    <input type="text" name="last_name" required><br>
 
-        <label for="username">Nom d'utilisateur :</label>
-        <input type="text" name="username" required><br>
+    <label for="username">Nom d'utilisateur :</label>
+    <input type="text" name="username" required><br>
 
-        <label for="password">Mot de passe :</label>
-        <input type="password" name="password" required><br>
+    <label for="password">Mot de passe :</label>
+    <input type="password" name="password" required><br>
 
-        <label for="email">Email :</label>
-        <input type="email" name="email" required><br>
+    <label for="email">Email :</label>
+    <input type="email" name="email" required><br>
 
-        <input type="submit" value="Créer le compte élève">
-    </form>
+    <input type="submit" value="Créer le compte élève">
+</form>
 
-    <!-- Lien pour se déconnecter -->
-    <a href='logout.php'>Se déconnecter</a>
-    </body>
+<!-- Lien pour se déconnecter -->
+<a href='logout.php'>Se déconnecter</a>
+</body>
 </html>
