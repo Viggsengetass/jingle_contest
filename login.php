ea90270b1c7136d8186e7c0bf32ea2d316b57a6b
Viggsengetass <paulito.antoine@gmail.com>
2023-08-10 11:26:01 +0200

    majj

--- login.php
+++ login.php
@@ -2,34 +2,20 @@
 session_start();
 require_once('config.php');
 
-// Vérifiez si l'utilisateur est déjà connecté, s'il l'est, redirigez-le vers le tableau de bord approprié
-if (isset($_SESSION['user_id'])) {
-    if ($_SESSION['role'] === 'student') {
-        header('Location: student_dashboard.php');
-    } elseif ($_SESSION['role'] === 'teacher') {
-        header('Location: teacher_dashboard.php');
-    }
-    exit();
-}
-
-// Traitement du formulaire de connexion
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Requête pour récupérer les informations de l'utilisateur à partir de la base de données
-    $sql = "SELECT * FROM users WHERE username='$username'";
-    $result = mysqli_query($conn, $sql);
+    $query = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($result) === 1) {
         $user = mysqli_fetch_assoc($result);
         if (password_verify($password, $user['password'])) {
-            // Connexion réussie, définir les informations de l'utilisateur dans la session
             $_SESSION['user_id'] = $user['user_id'];
             $_SESSION['username'] = $user['username'];
             $_SESSION['role'] = $user['role'];
 
-            // Rediriger vers le tableau de bord approprié selon le rôle de l'utilisateur
             if ($user['role'] === 'student') {
                 header('Location: student_dashboard.php');
             } elseif ($user['role'] === 'teacher') {
@@ -37,10 +23,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
             exit();
         } else {
-            $error_message = "Nom d'utilisateur ou mot de passe incorrect.";
+            $error_message = "Mot de passe incorrect.";
         }
     } else {
-        $error_message = "Nom d'utilisateur ou mot de passe incorrect.";
+        $error_message = "Nom d'utilisateur incorrect.";
     }
 }
 ?>
@@ -61,6 +47,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <input type="submit" value="Se connecter">
 </form>
-<p>Pas encore de compte ? <a href="register.php">Créer un compte</a></p>
+<p><a href="register.php">S'inscrire</a></p>
+<?php
+if (isset($error_message)) {
+    echo "<p>{$error_message}</p>";
+}
+?>
 </body>
 </html>
--- register.php
+++ register.php
@@ -2,34 +2,17 @@
 session_start();
 require_once('config.php');
 
-// Vérifiez si l'utilisateur est déjà connecté, s'il l'est, redirigez-le vers le tableau de bord approprié
-if (isset($_SESSION['user_id'])) {
-    if ($_SESSION['role'] === 'student') {
-        header('Location: student_dashboard.php');
-    } elseif ($_SESSION['role'] === 'teacher') {
-        header('Location: teacher_dashboard.php');
-    }
-    exit();
-}
-
-// Traitement du formulaire d'inscription
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
     $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     $role = $_POST['role'];
 
-    // Requête pour insérer les informations de l'utilisateur dans la base de données
-    $sql = "INSERT INTO users (first_name, last_name, username, password, email, role) 
-            VALUES ('$first_name', '$last_name', '$username', '$password', '$email', '$role')";
-
-    if (mysqli_query($conn, $sql)) {
-        header('Location: login.php');
-        exit();
+    $query = "INSERT INTO users (username, password, role) VALUES ('$username', '$hashed_password', '$role')";
+    if (mysqli_query($conn, $query)) {
+        $success_message = "Inscription réussie !";
     } else {
-        $error_message = "Une erreur est survenue lors de l'inscription.";
+        $error_message = "Erreur lors de l'inscription.";
     }
 }
 ?>
@@ -42,20 +25,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
 <h1>Inscription</h1>
 <form method="post" action="register.php">
-    <label for="first_name">Prénom :</label>
-    <input type="text" name="first_name" required><br>
-
-    <label for="last_name">Nom :</label>
-    <input type="text" name="last_name" required><br>
-
     <label for="username">Nom d'utilisateur :</label>
     <input type="text" name="username" required><br>
 
     <label for="password">Mot de passe :</label>
     <input type="password" name="password" required><br>
-
-    <label for="email">Email :</label>
-    <input type="email" name="email" required><br>
 
     <label for="role">Rôle :</label>
     <select name="role" required>
@@ -65,10 +39,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <input type="submit" value="S'inscrire">
 </form>
-<p>Déjà un compte ? <a href="login.php">Se connecter</a></p>
+<p><a href="login.php">Se connecter</a></p>
 <?php
 if (isset($error_message)) {
     echo "<p>{$error_message}</p>";
+}
+if (isset($success_message)) {
+    echo "<p>{$success_message}</p>";
 }
 ?>
 </body>
