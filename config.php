f0c6b077a6e87ee45dc17d44f8f2bc4f3507616a
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-11 14:20:43 +0200

    delete_config_dump

--- config.php
+++ config.php
@@ -7,12 +7,12 @@ define('DB_NAME', 'jingle');
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-if (!$conn) {
-    die("ERREUR : Impossible de se connecter à la base de données. " . mysqli_connect_error());
-}
- elseif ($conn) {
-    echo "Connexion réussie";
-} else {
-    echo "jsp moi";
-}
+//if (!$conn) {
+//    die("ERREUR : Impossible de se connecter à la base de données. " . mysqli_connect_error());
+//}
+// elseif ($conn) {
+//    echo "Connexion réussie";
+//} else {
+//    echo "jsp moi";
+//}
 ?>
