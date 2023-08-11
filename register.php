39234bdca476195c5ce946b1dfcf806e4a807d6f
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-11 15:37:44 +0200

    add_style_register

--- register.php
+++ register.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $role = $_POST['role'];
 
     // Requête pour insérer les informations de l'utilisateur dans la base de données
-    $sql = "INSERT INTO users (first_name, last_name, username, password, email, role) 
+    $sql = "INSERT INTO users (first_name, last_name, username, password, email, role)
             VALUES ('".$first_name."', '".$last_name."', '".$username."', '".$password."', '".$email."', '".$role."')";
 
     if (mysqli_query($conn, $sql)) {
@@ -38,38 +38,54 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>Inscription</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/style/register.css">
 </head>
 <body>
-<h1>Inscription</h1>
-<form method="post" action="register.php">
-    <label for="first_name">Prénom :</label>
-    <input type="text" name="first_name" required><br>
+<div class="register-container">
+    <h1 class="text-center">Inscription</h1>
+    <form method="post" action="register.php">
+        <div class="form-group">
+            <label for="first_name">Prénom :</label>
+            <input type="text" name="first_name" class="form-control" required>
+        </div>
 
-    <label for="last_name">Nom :</label>
-    <input type="text" name="last_name" required><br>
+        <div class="form-group">
+            <label for="last_name">Nom :</label>
+            <input type="text" name="last_name" class="form-control" required>
+        </div>
 
-    <label for="username">Nom d'utilisateur :</label>
-    <input type="text" name="username" required><br>
+        <div class="form-group">
+            <label for="username">Nom d'utilisateur :</label>
+            <input type="text" name="username" class="form-control" required>
+        </div>
 
-    <label for="password">Mot de passe :</label>
-    <input type="password" name="password" required><br>
+        <div class="form-group">
+            <label for="password">Mot de passe :</label>
+            <input type="password" name="password" class="form-control" required>
+        </div>
 
-    <label for="email">Email :</label>
-    <input type="email" name="email" required><br>
+        <div class="form-group">
+            <label for="email">Email :</label>
+            <input type="email" name="email" class="form-control" required>
+        </div>
 
-    <label for="role">Rôle :</label>
-    <select name="role" required>
-        <option value="student">Étudiant</option>
-        <option value="teacher">Enseignant</option>
-    </select><br>
+        <div class="form-group">
+            <label for="role">Rôle :</label>
+            <select name="role" class="form-control" required>
+                <option value="student">Étudiant</option>
+                <option value="teacher">Enseignant</option>
+            </select>
+        </div>
 
-    <input type="submit" value="S'inscrire">
-</form>
-<p>Déjà un compte ? <a href="login.php">Se connecter</a></p>
-<?php
-if (isset($error_message)) {
-    echo "<p>{$error_message}</p>";
-}
-?>
+        <input type="submit" value="S'inscrire" class="btn btn-primary btn-block">
+    </form>
+    <p class="text-center mt-3">Déjà un compte ? <a href="login.php">Se connecter</a></p>
+    <?php
+    if (isset($error_message)) {
+        echo "<p class='text-danger mt-3'>{$error_message}</p>";
+    }
+    ?>
+</div>
 </body>
 </html>
