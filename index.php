7b8a0e6cb5fb1f1fb60a3db3811845a3bb1e3013
Viggsengetass <paulito.antoine@gmail.com>
2023-08-16 16:19:49 +0200

    maj_index

--- index.php
+++ index.php
@@ -1,80 +1,50 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Concours de Sonnerie</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-        .container {
-            text-align: center;
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
-        }
-        h1 {
-            color: #333;
-            margin-bottom: 20px;
-        }
-        label, select, input[type="text"], input[type="password"], input[type="email"], input[type="submit"] {
-            display: block;
-            margin-bottom: 10px;
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 3px;
-            font-size: 14px;
-        }
-        select, input[type="submit"] {
-            background-color: #333;
-            color: #fff;
-            border: none;
-            cursor: pointer;
-        }
-        input[type="submit"]:hover {
-            background-color: #555;
-        }
-    </style>
+    <title>Accueil - VotreSiteDeJingles</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/style/index.css">
 </head>
 <body>
-<div class="container">
-    <h1>Concours de Sonnerie</h1>
-    <form method="post" action="register.php">
-        <label for="first_name">Prénom :</label>
-        <input type="text" name="first_name" required>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">VotreSiteDeJingles</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Se connecter</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
-        <label for="last_name">Nom :</label>
-        <input type="text" name="last_name" required>
+<!-- Hero Section -->
+<section class="hero bg-primary text-white text-center py-5">
+    <div class="container">
+        <h1 class="display-4">Bienvenue sur VotreSiteDeJingles</h1>
+        <p class="lead">Découvrez et évaluez des jingles créés par nos élèves talentueux.</p>
+    </div>
+</section>
 
-        <label for="username">Nom d'utilisateur :</label>
-        <input type="text" name="username" required>
+<!-- Top Rated Jingles Section -->
+<section class="top-rated py-5">
+    <div class="container">
+        <h2 class="text-center mb-4">Jingles les mieux notés</h2>
+        <!-- Insérez ici le classement des jingles les mieux notés -->
+    </div>
+</section>
 
-        <label for="password">Mot de passe :</label>
-        <input type="password" name="password" required>
+<!-- Footer -->
+<footer class="bg-light text-center py-3">
+    <p>&copy; 2023 VotreSiteDeJingles. Tous droits réservés.</p>
+</footer>
 
-        <label for="email">Email :</label>
-        <input type="email" name="email" required>
-
-        <label for="role">Rôle :</label>
-        <select name="role" required>
-            <option value="">Sélectionner un rôle</option>
-            <option value="student">Élève</option>
-            <option value="teacher">Professeur</option>
-        </select>
-
-        <input type="submit" value="S'inscrire">
-    </form>
-    <p>Déjà un compte ? <a href="login.php">Se connecter</a></p>
-</div>
+<!-- Scripts -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
