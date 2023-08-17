5e2c8c1c04db932c40f559d30d33a8166aee243c
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-17 16:43:43 +0200

    index

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Animation des Lettres</title>
+    <title>Accueil</title>
     <link rel="stylesheet" href="/style/test.css">
 </head>
 <body>
 <?php include 'header.php'; ?>
-<p aria-label="CodePen">
-    <span data-text="J">J</span>
-    <span data-text="I">I</span>
-    <span data-text="N">N</span>
-    <span data-text="G">G</span>
-    <span data-text="L">L</span>
-    <span data-text="E">E</span>
-    <span data-text="-">-</span>
-    <span data-text="C">C</span>
-    <span data-text="O">O</span>
-    <span data-text="N">N</span>
-    <span data-text="T">T</span>
-    <span data-text="E">E</span>
-    <span data-text="S">S</span>
-    <span data-text="T">T</span>
-</p>
+
+<div id="global">
+    <p aria-label="CodePen">
+        <span data-text="J">J</span>
+        <span data-text="I">I</span>
+        <span data-text="N">N</span>
+        <span data-text="G">G</span>
+        <span data-text="L">L</span>
+        <span data-text="E">E</span>
+        <span data-text="-">-</span>
+        <span data-text="C">C</span>
+        <span data-text="O">O</span>
+        <span data-text="N">N</span>
+        <span data-text="T">T</span>
+        <span data-text="E">E</span>
+        <span data-text="S">S</span>
+        <span data-text="T">T</span>
+    </p>
+</div>
 </body>
 </html>
--- test.php
+++ test.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Animation des Lettres</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/style/header.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <title>Accueil</title>
     <link rel="stylesheet" href="/style/test.css">
 </head>
 <body>
