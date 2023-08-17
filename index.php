f150bf436165b03d3fca62b364a5c95461c5c0c2
Viggsengetass <paulito.antoine@gmail.com>
2023-08-17 12:57:50 +0200

    test

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="/style/index.css">
@@ -35,32 +33,31 @@
     </div>
 </nav>
 
-<div class="container text-center mt-5">
-    <div class="animated fadeInDown">
-        <p aria-label="CodePen">
-            <span data-text="J">J</span>
-            <span data-text="I">I</span>
-            <span data-text="N">N</span>
-            <span data-text="G">G</span>
-            <span data-text="L">L</span>
-            <span data-text="E">E</span>
-            <span data-text="-">-</span>
-            <span data-text="C">C</span>
-            <span data-text="O">O</span>
-            <span data-text="N">N</span>
-            <span data-text="T">T</span>
-            <span data-text="E">E</span>
-            <span data-text="S">S</span>
-            <span data-text="T">T</span>
-        </p>
-    </div>
+<div class="text-animation-container">
+    <p class="text-animation" aria-label="CodePen">
+        <span data-text="J">J</span>
+        <span data-text="I">I</span>
+        <span data-text="N">N</span>
+        <span data-text="G">G</span>
+        <span data-text="L">L</span>
+        <span data-text="E">E</span>
+        <span data-text="-">-</span>
+        <span data-text="C">C</span>
+        <span data-text="O">O</span>
+        <span data-text="N">N</span>
+        <span data-text="T">T</span>
+        <span data-text="E">E</span>
+        <span data-text="S">S</span>
+        <span data-text="T">T</span>
+    </p>
+</div>
 
-    <div class="mt-4 animated fadeInUp">
+<div class="container mt-5 main-content">
+    <div class="text-center mb-4">
         <h1 class="display-4">Bienvenue sur Ma Plateforme</h1>
         <p class="lead">Découvrez et évaluez les jingles soumis par les élèves dans notre concours de création musicale !</p>
     </div>
-
-    <div class="mt-3 animated fadeIn">
+    <div class="text-center mt-3">
         <a href="ranking.php" class="btn btn-primary btn-lg">Voir le classement</a>
     </div>
 </div>
