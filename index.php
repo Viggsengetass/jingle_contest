36cc5ea3f7b81e241e6b99302a59e0f3e5d1caa7
Viggsengetass <paulito.antoine@gmail.com>
2023-08-17 13:56:50 +0200

    lfkddl

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <title>Accueil</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/style/index.css">
+    <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 </head>
 <body>
@@ -36,20 +36,9 @@
 <div class="container">
     <div class="text-animation-container">
         <p class="text-animation" aria-label="CodePen">
-            <span data-text="J">J</span>
-            <span data-text="I">I</span>
-            <span data-text="N">N</span>
-            <span data-text="G">G</span>
-            <span data-text="L">L</span>
-            <span data-text="E">E</span>
-            <span data-text="-">-</span>
-            <span data-text="C">C</span>
-            <span data-text="O">O</span>
-            <span data-text="N">N</span>
-            <span data-text="T">T</span>
-            <span data-text="E">E</span>
-            <span data-text="S">S</span>
-            <span data-text="T">T</span>
+            <span class="animate-text" data-text="Jingle">Jingle</span><br>
+            <span class="animate-text" data-text="-">-</span><br>
+            <span class="animate-text" data-text="Contest">Contest</span>
         </p>
     </div>
 
