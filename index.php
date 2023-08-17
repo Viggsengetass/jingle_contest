2ecb51896327984f2ed647e34e8ebc14a6083948
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-17 15:41:41 +0200

    revert_index

--- index.php
+++ index.php
@@ -1,44 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Accueil</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/style/index.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <title>Animation des Lettres</title>
+    <link rel="stylesheet" href="/style/test.css">
 </head>
 <body>
 <?php include 'header.php'; ?>
-<div class="container">
-    <div class="text-animation-container">
-        <p class="text-animation" aria-label="CodePen">
-            <span data-text="J">J</span>
-            <span data-text="I">I</span>
-            <span data-text="N">N</span>
-            <span data-text="G">G</span>
-            <span data-text="L">L</span>
-            <span data-text="E">E</span>
-            <span data-text="-">-</span>
-            <span data-text="C">C</span>
-            <span data-text="O">O</span>
-            <span data-text="N">N</span>
-            <span data-text="T">T</span>
-            <span data-text="E">E</span>
-            <span data-text="S">S</span>
-            <span data-text="T">T</span>
-        </p>
-    </div>
-
-    <div class="row justify-content-center mt-5">
-        <div class="col-md-12 text-center main-content">
-            <h1 class="display-4">Bienvenue sur Ma Plateforme</h1>
-            <p class="lead">Découvrez et évaluez les jingles soumis par les élèves dans notre concours de création musicale !</p>
-            <a href="ranking.php" class="btn btn-primary btn-lg">Voir le classement</a>
-        </div>
-    </div>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<p aria-label="CodePen">
+    <span data-text="J">J</span>
+    <span data-text="I">I</span>
+    <span data-text="N">N</span>
+    <span data-text="G">G</span>
+    <span data-text="L">L</span>
+    <span data-text="E">E</span>
+    <span data-text="-">-</span>
+    <span data-text="C">C</span>
+    <span data-text="O">O</span>
+    <span data-text="N">N</span>
+    <span data-text="T">T</span>
+    <span data-text="E">E</span>
+    <span data-text="S">S</span>
+    <span data-text="T">T</span>
+</p>
 </body>
 </html>
