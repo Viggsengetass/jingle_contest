31b5795b66d786be64be5ad08b6c3c4ba516211b
Viggsengetass <paulito.antoine@gmail.com>
2023-08-17 13:24:03 +0200

    gyu

--- index.php
+++ index.php
@@ -33,32 +33,34 @@
     </div>
 </nav>
 
-<div class="text-animation-container">
-    <p class="text-animation" aria-label="CodePen">
-        <span data-text="J">J</span>
-        <span data-text="I">I</span>
-        <span data-text="N">N</span>
-        <span data-text="G">G</span>
-        <span data-text="L">L</span>
-        <span data-text="E">E</span>
-        <span data-text="-">-</span>
-        <span data-text="C">C</span>
-        <span data-text="O">O</span>
-        <span data-text="N">N</span>
-        <span data-text="T">T</span>
-        <span data-text="E">E</span>
-        <span data-text="S">S</span>
-        <span data-text="T">T</span>
-    </p>
-</div>
-
-<div class="container mt-5 main-content">
-    <div class="text-center mb-4">
-        <h1 class="display-4">Bienvenue sur Ma Plateforme</h1>
-        <p class="lead">Découvrez et évaluez les jingles soumis par les élèves dans notre concours de création musicale !</p>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 text-center mt-5">
+            <p class="text-animation" aria-label="CodePen">
+                <span data-text="J">J</span>
+                <span data-text="I">I</span>
+                <span data-text="N">N</span>
+                <span data-text="G">G</span>
+                <span data-text="L">L</span>
+                <span data-text="E">E</span>
+                <span data-text="-">-</span>
+                <span data-text="C">C</span>
+                <span data-text="O">O</span>
+                <span data-text="N">N</span>
+                <span data-text="T">T</span>
+                <span data-text="E">E</span>
+                <span data-text="S">S</span>
+                <span data-text="T">T</span>
+            </p>
+        </div>
     </div>
-    <div class="text-center mt-3">
-        <a href="ranking.php" class="btn btn-primary btn-lg">Voir le classement</a>
+
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-12 text-center">
+            <h1 class="display-4">Bienvenue sur Ma Plateforme</h1>
+            <p class="lead">Découvrez et évaluez les jingles soumis par les élèves dans notre concours de création musicale !</p>
+            <a href="ranking.php" class="btn btn-primary btn-lg">Voir le classement</a>
+        </div>
     </div>
 </div>
 
