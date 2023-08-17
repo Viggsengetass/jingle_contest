0a1ea03670857ad7d41185d47a04e8725ca7f2d2
Viggsengetass <paulito.antoine@gmail.com>
2023-08-17 13:34:05 +0200

    fix_css

--- index.php
+++ index.php
@@ -34,29 +34,27 @@
 </nav>
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12 text-center mt-5">
-            <p class="text-animation" aria-label="CodePen">
-                <span data-text="J">J</span>
-                <span data-text="I">I</span>
-                <span data-text="N">N</span>
-                <span data-text="G">G</span>
-                <span data-text="L">L</span>
-                <span data-text="E">E</span>
-                <span data-text="-">-</span>
-                <span data-text="C">C</span>
-                <span data-text="O">O</span>
-                <span data-text="N">N</span>
-                <span data-text="T">T</span>
-                <span data-text="E">E</span>
-                <span data-text="S">S</span>
-                <span data-text="T">T</span>
-            </p>
-        </div>
+    <div class="text-animation-container">
+        <p class="text-animation" aria-label="CodePen">
+            <span data-text="J">J</span>
+            <span data-text="I">I</span>
+            <span data-text="N">N</span>
+            <span data-text="G">G</span>
+            <span data-text="L">L</span>
+            <span data-text="E">E</span>
+            <span data-text="-">-</span>
+            <span data-text="C">C</span>
+            <span data-text="O">O</span>
+            <span data-text="N">N</span>
+            <span data-text="T">T</span>
+            <span data-text="E">E</span>
+            <span data-text="S">S</span>
+            <span data-text="T">T</span>
+        </p>
     </div>
 
     <div class="row justify-content-center mt-5">
-        <div class="col-md-12 text-center">
+        <div class="col-md-12 text-center main-content">
             <h1 class="display-4">Bienvenue sur Ma Plateforme</h1>
             <p class="lead">Découvrez et évaluez les jingles soumis par les élèves dans notre concours de création musicale !</p>
             <a href="ranking.php" class="btn btn-primary btn-lg">Voir le classement</a>
