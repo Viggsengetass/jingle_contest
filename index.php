ad90c072110f5d25175c84ae6d8ea47045036725
Viggsengetass <paulito.antoine@gmail.com>
2023-08-17 10:48:09 +0200

    trtrt

--- index.php
+++ index.php
@@ -1,55 +1,47 @@
-<?php
-session_start();
-require_once('config.php');
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Accueil</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/style/index.css">
-
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-<!-- Barre de tâches -->
-<div class="navbar">
-    <div class="navbar-inner">
-        <div class="container">
-            <ul class="nav">
-                <li><a href="index.php">Accueil</a></li>
-                <?php
-                // Afficher le lien vers le tableau de bord approprié en fonction du rôle de l'utilisateur
-                if (isset($_SESSION['role']) && $_SESSION['role'] === 'student') {
-                    echo '<li><a href="student_dashboard.php">Tableau de bord étudiant</a></li>';
-                } elseif (isset($_SESSION['role']) && $_SESSION['role'] === 'teacher') {
-                    echo '<li><a href="teacher_dashboard.php">Tableau de bord professeur</a></li>';
-                }
-                ?>
-                <li><a href="public_rankings.php">Classement Public</a></li>
-                <?php
-                // Afficher le lien de déconnexion si l'utilisateur est connecté
-                if (isset($_SESSION['user_id'])) {
-                    echo '<li><a href="logout.php">Se déconnecter</a></li>';
-                } else {
-                    echo '<li><a href="login.php">Se connecter</a></li>';
-                }
-                ?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Ma Plateforme</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="student_dashboard.php">Tableau de bord élève</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="teacher_dashboard.php">Tableau de bord professeur</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Se connecter</a>
+                </li>
             </ul>
         </div>
     </div>
-</div>
+</nav>
 
 <div class="container mt-5">
-    <h1>Page d'accueil</h1>
-    <p>Bienvenue sur notre plateforme de soumission de jingles !</p>
-    <?php
-    // Afficher un message de bienvenue si l'utilisateur est connecté
-    if (isset($_SESSION['user_id'])) {
-        echo "<p>Bienvenue, {$_SESSION['username']} !</p>";
-    }
-    ?>
+    <h1 class="text-center">Bienvenue sur Ma Plateforme</h1>
+    <p class="text-center">Découvrez les jingles soumis par les élèves et consultez le classement.</p>
+    <div class="text-center mt-3">
+        <a href="ranking.php" class="btn btn-primary">Voir le classement</a>
+    </div>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
