623d6fb0345f740850055d967113064dcdce8ffc
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-17 15:38:07 +0200

    supp

--- index.php
+++ index.php
@@ -8,32 +8,6 @@
 </head>
 <body>
 <?php include 'header.php'; ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="#">Ma Plateforme</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="student_dashboard.php">Tableau de bord élève</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="teacher_dashboard.php">Tableau de bord professeur</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="login.php">Se connecter</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
 <div class="container">
     <div class="text-animation-container">
         <p class="text-animation" aria-label="CodePen">
