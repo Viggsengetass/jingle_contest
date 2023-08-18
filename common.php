d7635ae40c59a73609d845a4f8e31492e5f2f8a8
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-18 12:41:09 +0200

    add_common_css

--- common.php
+++ common.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="/style/header.css">
+<link rel="stylesheet" href="/style/common.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
