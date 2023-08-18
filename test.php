74c79ea46515d6d89c4aeb85684e6eee9c6c32bb
Viggsengetass <paulito.antoine@gmail.com>
2023-08-18 10:57:43 +0200

    add_query

--- test.php
+++ test.php
@@ -3,8 +3,8 @@
 <head>
     <title>Accueil</title>
     <link rel="stylesheet" href="/style/test.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="/js/test.js"></script>
-
 </head>
 <body>
 
