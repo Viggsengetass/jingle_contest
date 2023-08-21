ca5e332f8d5da28ddb2d618e226255fee9c2398b
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-21 14:39:25 +0200

    zez

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
      <img src="assets/img/remarques.png" alt=""  <a class="navbar-toggler-icon" href="index.php"></a> >
-        <a class="navbar-brand" href="#">Jingle-contest</a>
+        <a class="navbar-brand" href="index.php">Jingle-contest</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
