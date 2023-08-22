0c7486ddf406b0af7d8840624d0835d487a926ff
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-22 12:56:27 +0200

    <<SEEFZE

--- common.php
+++ common.php
@@ -4,5 +4,3 @@
 <link rel="stylesheet" href="/style/common.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/howler@2.2.0/dist/howler.min.js"></script>
-
