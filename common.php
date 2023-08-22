94b1980f51278f889a6d73a8b01f22a6a05af8cb
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-22 12:54:37 +0200

    Revert "<<S"
    This reverts commit a3e0f2fab300d376e37fc8bd569f0ddd5221d28e.

--- common.php
+++ common.php
@@ -1,7 +1,8 @@
-<link rel="icon" href="/assets/img/remarques.png " />
+<link rel="icon" href="/assets/img/remarques.png" />
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="/style/header.css">
 <link rel="stylesheet" href="/style/common.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/howler@2.2.0/dist/howler.min.js"></script>
+
