96d95b4597eebf1b17b0f2e87128587dd6d0f663
Paul ANTOINE 287152 <116275907+Viggsengetass@users.noreply.github.com>
2023-08-22 14:09:02 +0200

    FIX8CONFLICT

--- ranking.php
+++ ranking.php
@@ -14,9 +14,12 @@ $result = mysqli_query($conn, $query);
 <html>
 <head>
     <title>Classement public des jingles par notation</title>
+    <?php include 'common.php'; ?>
     <link rel="stylesheet" href="/style/ranking.css">
 </head>
 <body>
+<?php include 'header.php'; ?>
+
 <div class="container mt-5">
     <h1>Classement public des jingles par notation</h1>
     <table class="table table-bordered">
@@ -40,21 +43,10 @@ $result = mysqli_query($conn, $query);
             echo "<td>{$row['username']}</td>"; // Nom de l'utilisateur
             $average_score = isset($row['average_score']) ? round($row['average_score'], 2) : "Aucune note";
             echo "<td>{$average_score}</td>";
-            echo '<td class="audio-player">';
-            echo "<audio controls>";
+            echo "<td><audio controls>";
             echo "<source src='{$row['jingle_file_path']}' type='audio/mpeg'>";
             echo "Votre navigateur ne prend pas en charge l'élément audio.";
-            echo "</audio>";
-            echo "<button class='play-pause-button' data-playing='false'>▶</button>"; // Bouton de lecture/pause personnalisé
-            echo "<div class='timeline-container'>";
-            echo "<div class='timeline'>";
-            echo "<div class='progress-bar'></div>";
-            echo "</div>";
-            echo "<div class='timer'>00:00</div>";
-            echo "</div>";
-            echo "<button class='speed-button'>1x</button>"; // Bouton pour changer la vitesse de lecture
-            echo "<a class='download-link' href='{$row['jingle_file_path']}' download>Télécharger</a>"; // Lien de téléchargement
-            echo "</td>";
+            echo "</audio></td>";
             echo "</tr>";
             $rank++;
         }
@@ -63,7 +55,5 @@ $result = mysqli_query($conn, $query);
     </table>
     <a class="btn btn-primary" href="index.php">Retour à la page d'accueil</a>
 </div>
-
-<script src="/js/ranking.js"></script>
 </body>
 </html>
