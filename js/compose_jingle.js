b114b1ba44edde2d9df9b8f46e5aaf149e471682
Viggsengetass <paulito.antoine@gmail.com>
2023-08-23 09:44:13 +0200

    fix

--- js/compose_jingle.js
+++ js/compose_jingle.js
@@ -1,25 +1,15 @@
 let isRecording = false;
-let audioChunks = [];
 let mediaRecorder;
-let audioStream;
-let audioContext;
-let mp3Data = [];
+let audioChunks = [];
 
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
 const downloadLink = document.getElementById('downloadLink');
 
 startButton.addEventListener('click', async () => {
-    isRecording = true;
-    startButton.disabled = true;
-    stopButton.disabled = false;
-    downloadLink.style.display = 'none';
-    audioChunks = [];
-    mp3Data = [];
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-    audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    mediaRecorder = new MediaRecorder(audioStream);
+    mediaRecorder = new MediaRecorder(stream);
 
     mediaRecorder.ondataavailable = event => {
         if (event.data.size > 0) {
@@ -28,40 +18,41 @@ startButton.addEventListener('click', async () => {
     };
 
     mediaRecorder.onstop = () => {
-        isRecording = false;
-        startButton.disabled = false;
-        stopButton.disabled = true;
-        downloadLink.style.display = 'block';
-
         const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
 
         const reader = new FileReader();
         reader.onload = async () => {
             const wavData = new Uint8Array(reader.result);
+
             convertWavToMp3(wavData, (mp3Array) => {
-                mp3Data = new Uint8Array(mp3Array);
-                const mp3Blob = new Blob([mp3Data], { type: 'audio/mp3' });
+                const mp3Blob = new Blob([mp3Array], { type: 'audio/mp3' });
                 const mp3Url = URL.createObjectURL(mp3Blob);
                 downloadLink.href = mp3Url;
                 downloadLink.download = 'jingle.mp3';
                 downloadLink.style.display = 'block';
             });
         };
+
         reader.readAsArrayBuffer(audioBlob);
     };
 
     mediaRecorder.start();
+    isRecording = true;
+    startButton.disabled = true;
+    stopButton.disabled = false;
 });
 
 stopButton.addEventListener('click', () => {
     if (isRecording && mediaRecorder) {
         mediaRecorder.stop();
-        audioStream.getTracks().forEach(track => track.stop());
+        isRecording = false;
+        startButton.disabled = false;
+        stopButton.disabled = true;
     }
 });
 
 function convertWavToMp3(wavData, callback) {
-    const encoder = new lamejs.Mp3Encoder(1, audioContext.sampleRate, 128);
+    const encoder = new lamejs.Mp3Encoder(1, 44100, 128);
     const samples = new Int16Array(wavData.buffer);
 
     const mp3Data = [];
