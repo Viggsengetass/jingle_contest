e2c2aed4a16ca2a652b5ebffb07e5af9b2461568
Viggsengetass <paulito.antoine@gmail.com>
2023-08-23 09:22:52 +0200

    ccc

--- compose_jingle.php
+++ compose_jingle.php
@@ -1,20 +1,16 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Composition de Jingle</title>
-    <?php include 'common.php'; ?>
-    <link rel="stylesheet" href="/style/compose_jingle.css">
 </head>
 <body>
 <h1>Composition de Jingle</h1>
 <button id="startButton">Démarrer la Composition</button>
 <button id="stopButton" disabled>Arrêter la Composition</button>
-<button id="downloadButton" disabled>Télécharger le Jingle (MP3)</button>
+<a id="downloadLink" style="display: none;">Télécharger le Jingle (MP3)</a>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.26/Tone.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lamejs/1.2.0/lame.min.js"></script>
 <script src="/js/compose_jingle.js"></script>
 </body>
 </html>
--- js/compose_jingle.js
+++ js/compose_jingle.js
@@ -1,79 +1,49 @@
 let isRecording = false;
-let recorder, audioContext;
 let audioChunks = [];
+let mediaRecorder;
 
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
-const downloadButton = document.getElementById('downloadButton');
+const downloadLink = document.getElementById('downloadLink');
 
-startButton.addEventListener('click', async () => {
-    audioChunks = [];
+startButton.addEventListener('click', () => {
     isRecording = true;
     startButton.disabled = true;
     stopButton.disabled = false;
+    downloadLink.style.display = 'none';
+    audioChunks = [];
 
-    audioContext = new AudioContext();
-    recorder = new MediaRecorder(audioContext.createMediaStreamDestination());
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            mediaRecorder = new MediaRecorder(stream);
 
-    recorder.ondataavailable = event => audioChunks.push(event.data);
-    recorder.onstop = () => {
-        isRecording = false;
-        startButton.disabled = false;
-        stopButton.disabled = true;
-        downloadButton.disabled = false;
-    };
+            mediaRecorder.ondataavailable = event => {
+                if (event.data.size > 0) {
+                    audioChunks.push(event.data);
+                }
+            };
 
-    recorder.start();
+            mediaRecorder.onstop = () => {
+                isRecording = false;
+                startButton.disabled = false;
+                stopButton.disabled = true;
+                downloadLink.style.display = 'block';
+
+                const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+                const audioUrl = URL.createObjectURL(audioBlob);
+                downloadLink.href = audioUrl;
+                downloadLink.download = 'jingle.wav';
+            };
+
+            mediaRecorder.start();
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération de l\'accès au microphone:', error);
+        });
 });
 
 stopButton.addEventListener('click', () => {
-    if (isRecording) {
-        recorder.stop();
+    if (isRecording && mediaRecorder) {
+        mediaRecorder.stop();
     }
 });
-
-downloadButton.addEventListener('click', () => {
-    if (audioChunks.length > 0) {
-        const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
-        const reader = new FileReader();
-
-        reader.onload = async () => {
-            const wavData = new Uint8Array(reader.result);
-            const mp3Data = convertWavToMp3(wavData);
-
-            const mp3Blob = new Blob([mp3Data.buffer], { type: 'audio/mp3' });
-            const mp3Url = URL.createObjectURL(mp3Blob);
-
-            const downloadLink = document.createElement('a');
-            downloadLink.href = mp3Url;
-            downloadLink.download = 'jingle.mp3';
-            downloadLink.click();
-        };
-
-        reader.readAsArrayBuffer(audioBlob);
-    }
-});
-
-function convertWavToMp3(wavData) {
-    const encoder = new lamejs.Mp3Encoder(1, 44100, 128);
-    const mp3Data = [];
-
-    const samples = new Int16Array(wavData.buffer);
-    const sampleBlockSize = 1152;
-
-    for (let i = 0; i < samples.length; i += sampleBlockSize) {
-        const leftChunk = samples.subarray(i, i + sampleBlockSize);
-        const mp3Buffer = encoder.encodeBuffer(leftChunk);
-        if (mp3Buffer.length > 0) {
-            mp3Data.push(new Int8Array(mp3Buffer));
-        }
-    }
-
-    const mp3Buffer = encoder.flush();
-    if (mp3Buffer.length > 0) {
-        mp3Data.push(new Int8Array(mp3Buffer));
-    }
-
-    const result = new Uint8Array(mp3Data.reduce((acc, chunk) => acc.concat(Array.from(chunk)), []));
-    return result;
-}
